78bbf2ba62a9c3d9eafb41eaa056b58e6a41964f
lcorrales316 <138952616+lcorrales316@users.noreply.github.com>
2024-10-31 08:18:34 -0600

    Cambios profesor

--- JavaApplication1/src/NewClass.java
+++ JavaApplication1/src/NewClass.java
@@ -15,7 +15,7 @@ public class NewClass {
         Discrete_PSO_Swarm swarm = new Discrete_PSO_Swarm(funcionAdaptacion);
         swarm.init();
         
-        for(int i =0; i<10; i++){
+        for(int i =0; i<100; i++){
             swarm.evolve();
         }
         
@@ -23,4 +23,8 @@ public class NewClass {
         for(Allocation asignacion : swarm.getBestPosition())
             System.out.println(asignacion);
     }
+    
+    
+    
+    
 }
